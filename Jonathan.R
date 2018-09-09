63cf9bfb42985a2f6e3808864778cbb9b775f9ce
Jonathan <16845552+jonathanc42@users.noreply.github.com>
2018-09-08 23:19:20 -0400

    Jonathan code update and tried r notebook

--- Jonathan.R
+++ Jonathan.R
@@ -1,3 +1,11 @@
-library(openxlsx)
+load('LeukError.RData')
 
-data=load('LeukError.RData')
+table(leuk$V5001)
+
+class(leuk)
+
+# there are 38 labeled data not matching the email 37 
+sum(!is.na(leuk$V5001))
+
+# and they are not duplicated
+sum(duplicated(leuk))
